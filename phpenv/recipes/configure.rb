07d8768eafa8c0dc5233a2cb891c7474d8f11603
Rohan Dey <me@rohandey.com>
2013-05-17 07:13:44 +0530

    Update configure.rb

--- phpenv/recipes/configure.rb
+++ phpenv/recipes/configure.rb
@@ -1,5 +1,13 @@
 node[:deploy].each do |application, deploy|
-  
+  script "install_composer" do
+    interpreter "bash"
+    user "root"
+    cwd "#{deploy[:deploy_to]}/current"
+    code <<-EOH
+    curl -s https://getcomposer.org/installer | php
+    php composer.phar install
+    EOH
+  end  
   template "#{deploy[:deploy_to]}/current/.htaccess" do
     source "htaccess.erb"
     owner deploy[:user] 
